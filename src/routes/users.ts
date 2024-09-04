d2ae73e9b64ee749f0954d059bc54d99e12f0f56
Cleyton Albuquerque <cleyton.albuquerque@dcx.ufpb.br>
2024-09-04 16:07:44 -0300

    rotas de usuarios

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,29 +1,98 @@
 import { prisma } from './../lib/prisma';
 import { FastifyInstance } from "fastify";
-
+import { z } from 'zod'
 
 export async function usersRoutes(app: FastifyInstance){
+   
+    // GET Listar todos os usuarios
     app.get('/users', async ()=> {
         const users = await prisma.user.findMany()
 
         return users 
     })
 
+    // GET Listar usuarios pelo ID 
+    app.get('/users/:id', async (request)=> {
+        //validacao de ID 
+        const paramnsSchema = z.object({
+            id: z.string().uuid()
+        })
+        const { id } = paramnsSchema.parse(request.params)
 
-    app.get('/users/:id', async ()=> {
+        const user =  prisma.user.findFirstOrThrow({
+            where: {
+                id,
+            },
+        })
+        return user
         
     })
 
-    app.post('/users', async ()=>{
+    // Criar novo usuario
+    app.post('/users', async (request)=>{
+        //Validacao do corpo da requisicao
+        const bodySchema = z.object({
+            githubId: z.number(),
+            name: z.string(),
+            login: z.string(),
+            avatarUrl: z.string(),
+            
+        })
+
+        const { githubId, name, login, avatarUrl} = bodySchema.parse(request.body)
+
+        const user = await prisma.user.create({
+            data:{
+                githubId,
+                name,
+                login,
+                avatarUrl
+            }
+        })
+    })
+
+    // Alterar usuario por ID
+    app.put('/users/:id', async (request)=>{
+
+        // Validacao de ID
+        const paramnsSchema = z.object({id: z.string().uuid()})
+        // Validacao do corpo da requisicao 
+        const bodySchema = z.object({
+            githubId: z.number(),
+            name: z.string(),
+            login: z.string(),
+            avatarUrl: z.string()
+        })
+
+        const { id } = paramnsSchema.parse(request.params)
+        const {githubId, name, login, avatarUrl} = bodySchema.parse(request.body)
+        const user = await prisma.user.update({
+            where:{
+                id,
+            },
+            data: {
+                githubId,
+                name,
+                login, 
+                avatarUrl
+            }
+        })
+        return user
 
     })
 
-    app.put('/users/:id', async ()=>{
+    // Deletar usuario pelo ID
+    app.delete('/users/:id', async (request)=>{
+        //Validar ID
+        const paramnsSchema = z.object({id: z.string().uuid()})
 
-    })
-
-    app.delete('/users/:id', async ()=>{
-
+        const { id } = paramnsSchema.parse(request.params)
+        
+        await prisma.user.delete({
+            where:{
+                id,
+            }
+        })
     })
 
 }
